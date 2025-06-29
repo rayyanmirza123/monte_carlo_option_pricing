49d3e154f12d867bf7c8a8f4d37a9b5e865f9ccb
baphomet <rayyanmirza419@gmail.com>
2025-06-30 01:07:05 +0530

    european call/put options implementation
    european call/put options implementation

--- src/monte_carlo_sim.py
+++ src/monte_carlo_sim.py
@@ -9,3 +9,16 @@ def simulate_gbm(S0, mu, sigma, T, N, M):
         Z = np.random.normal(size=M)
         paths[:, t] = paths[:, t-1] * np.exp((mu - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * Z)
     return paths
+
+def price_option(S0, K, T, r, sigma, N, M, option_type='call'):
+    paths = simulate_gbm(S0, r, sigma, T, N, M)
+    ST = paths[:, -1]
+    if option_type == 'call':
+        payoffs = np.maximum(ST - K, 0)
+    elif option_type == 'put':
+        payoffs = np.maximum(K - ST, 0)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
+    discounted = np.exp(-r * T) * payoffs
+    return paths, np.mean(discounted)
+
